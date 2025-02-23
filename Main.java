bda2b3666f41ba9774a2e03df9372f26a337046b
202404407 <up202404407@up.pt>
2025-02-23 16:28:24 +0000

    Update Main.java
    Variables have been changed.
    Object c3 was created.

--- Main.java
+++ Main.java
@@ -3,18 +3,25 @@ public class Main {
         //c1 e c2 são instanciados
         Cilindro c1= new Cilindro();
         Cilindro c2 = new Cilindro();
+        Cilindro c3 = new Cilindro();
         double v1;
         double v2;
+        double v3;
 
-        c1.setRaio(5);
-        c1.setAltura(10);
+        c1.setRaio(8);
+        c1.setAltura(12);
         v1 = c1.calcularVolume(); //método de instância para determinar volume do c1
 
-        c2.setRaio(4);
-        c2.setAltura(16);
+        c2.setRaio(5);
+        c2.setAltura(14);
         v2 = c2.calcularVolume();
+
+        c3.setRaio(10);
+        c3.setAltura(18);
+        v3 = c3.calcularVolume();
 
         System.out.println("Volume de c1: " + v1);
         System.out.println("Volume de c2: " + v2);
+        System.out.println("Volume de c3: " + v3);
     }
 }
