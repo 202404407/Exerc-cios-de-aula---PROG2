c9811864b552793da779485811bd720d7f16f597
202404407 <up202404407@up.pt>
2025-02-23 17:37:26 +0000

    Update MainPessoas.java
    Object p2 was created

--- Aula1/MainPessoas.java
+++ Aula1/MainPessoas.java
@@ -3,6 +3,7 @@ package Aula1;
 public class MainPessoas {
     public static void main(String[] args) {
         Pessoas p1 = new Pessoas();
+        Pessoas p2 = new Pessoas();
 
         double indice;
         String name;
@@ -22,10 +23,23 @@ public class MainPessoas {
         weight = p1.weigth ();
         classification = p1.getClasseIMC();
 
-
-
-
         System.out.println ("A " + name + " tem " + age +" anos.\nEla mede " + heigth + "m e pesa " + weight + "kg." );
+        System.out.printf ("O seu índice é " + "%.2f.", indice);
+        System.out.println ("\nEste índice indica que está" + classification + ".");
+
+
+        p2.setNome("Jorge");
+        p2.setIdade(21);
+        p2.setAltura(1.75);
+        p2.setPeso(65);
+        indice = p2.calcularIndice();
+        name = p2.name();
+        age = p2.age();
+        heigth = p2.height();
+        weight = p2.weigth ();
+        classification = p2.getClasseIMC();
+
+        System.out.println ("O " + name + " tem " + age +" anos.\nEle mede " + heigth + "m e pesa " + weight + "kg." );
         System.out.printf ("O seu índice é " + "%.2f.", indice);
         System.out.println ("\nEste índice indica que está" + classification + ".");
     }
